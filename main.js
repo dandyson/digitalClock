db7b2f5d49f019cfbf6c0292ae5340333ef6cd10
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-03-25 19:16:27 +0100

    Improved code

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-const addZero = i => {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-};
-
 const showTime = () => {
   let time = document.getElementById("time");
   let d = new Date();
-  let n = d.getHours();
+  let h = d.getHours();
   let m = d.getMinutes();
   let s = d.getSeconds();
+  //Add extra zeros on single digits
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
   //Display time on the page
-  time.innerHTML = `${n}:${m}.${s}`;
+  time.innerHTML = `${h}:${m}:${s}`;
 };
 
-showTime();
 setInterval(showTime, 1000);
